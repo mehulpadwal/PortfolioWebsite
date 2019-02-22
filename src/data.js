c3462a30fcbccd68b8f01e486d3b8831f96bc256
Mehul Padwal <padwal.mehul7@gmail.com>
2019-02-22 05:09:59 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,94 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "http://twitter/"
+        },
+        {
+            "icon": "github-circled",
+            "url": "http://github.com/mehulpadwal"
+        },
+        {
+            "icon": "dribbble",
+            "url": "http://dribbble"
+        },
+        {
+            "icon": "skype",
+            "url": "htttp://skype"
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "https://www.linkedin.com/in/mehulpadwal/"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "13df8766-f677-40c5-bebc-70ed0beb526a",
+            "title": "Associate Software Developer at Accenture",
+            "timeline": "Nov 2016 - Nov 2017",
+            "description": "",
+            "responsibilities": [
+                "Worked on developing and improving client’s customer support management software on desktop and mobile",
+                "Developed a tool called Automatic Incident Resolver using NLTK to extract keywords and then feeding that keywords to\nKNN classifier that identifies new incidents and provides a solution.This helped reduce the effective manual workload by at least 15 percent..",
+                "Was responsible for automating redundant processes like generating day-to-day reports."
+            ]
+        },
+        {
+            "__key": "5db6f413-7099-40a7-8872-bae6101461a7",
+            "title": "Research Intern at Bhabha Atomic Research Center",
+            "timeline": "Spring 2016",
+            "responsibilities": [
+                "Researched on developing a cost-effective system that assists blind people to walk without the help of a cane",
+                "Came up with an effective solution by using an Android phone’s camera instead of expensive stereoscopic cameras and\nlow-priced Arduino chip that achieved 50 percent reduction in cost"
+            ],
+            "description": ""
+        }
+    ],
+    "name": "Mehul Padwal",
+    "header": "Hi, I'm Mehul Padwal",
+    "cta": {
+        "label": "Get my resume",
+        "url": "https://imgur.com/0ODaF36"
+    },
+    "testimonials": [
+        {}
+    ],
+    "title": "Mehul Padwal Portfolio",
+    "projects": [
+        {
+            "__key": "7d5ec1ad-7ef1-4051-859c-8e7b81a33a25",
+            "name": "Code Publisher Client",
+            "description": "▪ Building a GUI client that enables navigation and simultaneous viewing of project files.<br/>\n▪ Client also include remote page manager using message passing asynchronous communication channel to support file\nupload and deletion. Supports creation of popup windows with individual file contents.<br/>\n▪ Implementing in C++ using object-oriented design principles for large scale projects",
+            "tags": [
+                "C++",
+                "Visual Studio 2017",
+                "Object Oriented Design"
+            ],
+            "alt": "Code Screenshot",
+            "img": "cpp.png",
+            "url": "//"
+        },
+        {
+            "__key": "f8586d7e-6a57-45bd-bc2d-21e3ac327306",
+            "name": "Netflix Recommendation",
+            "description": "▪ Preprocessed Netflix data with 100 million data points using MapReduce to make user-movie matrix to predict movie\nratings given by a user. <br/>\n▪ Implemented machine learning modeling techniques like SVD, KNN, Correlations and Neural Networks to achieve best\nroot mean squared error of 0.916 <br/>\n▪ Combined the above models using ensemble methods to reduce error rate",
+            "tags": [
+                "Python3",
+                "Keras",
+                "Data Mining",
+                "Pandas",
+                "Sklearn"
+            ],
+            "img": "netflix.png",
+            "url": "//",
+            "alt": "netflix"
+        }
+    ],
+    "email": "padwal.mehul7@gmail.com",
+    "description": "I'm a Master's Student currently taking up Computer Science in at Syracuse University. I enjoy building everything from small business sites to rich interactive web apps",
+    "footerTitle": "Software Developer"
+}
